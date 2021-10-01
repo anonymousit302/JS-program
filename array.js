7d4e7ac04c1e0cbe4e1249a5251f5f29d3d79f07
anonymousit302 <69596536+anonymousit302@users.noreply.github.com>
2021-10-01 16:08:02 +0530

    Added part2 of JS

--- array.js
+++ array.js
@@ -21,3 +21,30 @@ console.log(user)
 console.log(user.indexOf(3))
 console.log(Array.from("abc"))
  
+/*
+function isEven(element){
+    // if (element %2 === 0){
+    //     return true
+    // }
+    // return false
+    return element % 2 === 0;
+} */
+var isEven = (element) => {
+
+    return element % 2 === 0;
+}
+// console.log(isEven(4))
+
+//Every perfrom operation on each element and give combine result 
+// var result = [2,3,6,8].every(isEven)
+// console.log(result)
+
+
+//callback function 
+var result = [2,4,6,8].every((element)=>(element%2 === 0 ));
+console.log(result)
+//if we want to return value in call back function we need {}
+var result = [2,3,3,8].every((element)=>{
+    return element%2 === 0;
+})
+console.log(result)
