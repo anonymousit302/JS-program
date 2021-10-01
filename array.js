1b0c44a325944aef0ae26641a9530b543de3381f
notes <72154714+notesit@users.noreply.github.com>
2021-10-01 16:06:39 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+var name1 ;
+var name2 ;
+var name3 ;
+
+var countries = ["India","Paris","nepal","butan"]
+
+var states =  new Array("goa","Delhi","mumbai")
+console.log(states[0])
+console.log(states.length)
+states[0]="maharashatra"
+console.log(states)
+
+var user = ["abc","abc@gmail.com",3,34,true]
+// console.log(user)
+user.pop()
+// console.log(user)
+user.unshift("New user name is golang")
+// console.log(user)
+user.shift()
+console.log(user)
+console.log(user.indexOf(3))
+console.log(Array.from("abc"))
+ 
